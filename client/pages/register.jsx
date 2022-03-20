4f5db25dc646f35efd2a2d584842da27c80ae545
Hans Micaya <hcmicaya@gmail.com>
2022-03-19 22:36:25 -0400

    added success/error, and register helpers

--- client/pages/register.jsx
+++ client/pages/register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Layout from "../components/Layout";
 import axios from "axios";
+import { showSuccessMessage, showErrorMessage } from "../helpers/alerts";
 
 const Register = () => {
     const [state, setState] = useState({
@@ -26,6 +27,7 @@ const Register = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setState({ ...state, buttonText: "Registering" });
         // console.table({ name, email, password });
         axios
             .post(`http://localhost:8000/api/register`, {
@@ -33,8 +35,25 @@ const Register = () => {
                 email,
                 password,
             })
-            .then((response) => console.log(response))
-            .catch((error) => console.log(error));
+            .then((response) => {
+                console.log(response);
+                setState({
+                    ...state,
+                    name: "hans",
+                    email: "hansmicaya@yahoo.com",
+                    password: "123123",
+                    buttonText: "Submitted",
+                    success: response.data.message,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+                setState({
+                    ...state,
+                    buttonText: "Register",
+                    error: error.response.data.error,
+                });
+            });
     };
 
     const registerForm = () => (
@@ -79,9 +98,9 @@ const Register = () => {
             <div className="col-md-6 offset-md-3">
                 <h1>Register</h1>
                 <br />
+                {success && showSuccessMessage(success)}
+                {error && showErrorMessage(error)}
                 {registerForm()}
-                <br />
-                {JSON.stringify(state)}
             </div>
         </Layout>
     );
