c8c2c23e54d170609adb2ba6f57d1c63b1a2ac55
Hans Micaya <hcmicaya@gmail.com>
2022-03-24 22:40:57 -0400

    Added category schema, route, and controllers. Add S3 form upload

---
+++ server/controllers/category.js
@@ -0,0 +1,97 @@
+const Category = require("../models/category");
+const slugify = require("slugify");
+const formidable = require("formidable");
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+
+// s3
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKeyId: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+
+exports.create = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Image could not upload",
+            });
+        }
+        // console.table({err, fields, files})
+        const { name, content } = fields;
+        const { image } = files;
+
+        const slug = slugify(name);
+        let category = new Category({ name, content, slug });
+        if (image.size > 2000000) {
+            return res.status(400).json({
+                error: "Image should be less than 2mb",
+            });
+        }
+        // upload image to s3
+        const params = {
+            Bucket: "bulletin613",
+            Key: `category/${uuidv4()}`,
+            Body: fs.readFileSync(image.filepath),
+            ACL: "public-read",
+            ContentType: `image/jpg`,
+        };
+
+        s3.upload(params, (err, data) => {
+            if (err) res.status(400).json({ error: "Upload to s3 failed." });
+            console.log("AWS UPLOAD RES DATA", data);
+            category.image.url = data.Location;
+            category.image.key = data.Key;
+
+            // save to db
+            category.save((err, success) => {
+                if (err)
+                    res.status(400).json({
+                        error: "Duplicate category",
+                    });
+                return res.json(success);
+            });
+        });
+    });
+};
+
+// exports.create = (req, res) => {
+//     const { name, content } = req.body;
+//     const slug = slugify(name);
+//     const image = {
+//         url: `https://via.placeholder.com/200x150.png?text=${process.env.CLIENT_URL}`,
+//         key: "123",
+//     };
+
+//     const category = new Category({ name, slug, image, content });
+//     category.posteBy = req.user._id;
+
+//     category.save((err, data) => {
+//         if (err) {
+//             console.log("CATEGORY CREATE ERR", err);
+//             return res.status(400).json({
+//                 error: "Category create failed",
+//             });
+//         }
+//         res.json(data);
+//     });
+// };
+
+exports.list = (req, res) => {
+    //
+};
+
+exports.read = (req, res) => {
+    //
+};
+
+exports.update = (req, res) => {
+    //
+};
+
+exports.remove = (req, res) => {
+    //
+};
---
+++ server/routes/category.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+
+// import validators
+const {
+    categoryCreateValidator,
+    categoryUpdateValidator,
+} = require("../validators/category");
+
+// controllers
+const {
+    requireSignin,
+    authMiddleware,
+    adminMiddleware,
+} = require("../controllers/auth");
+
+const { runValidation } = require("../validators");
+const {
+    create,
+    list,
+    read,
+    update,
+    remove,
+} = require("../controllers/category");
+
+// routes
+router.post(
+    "/category",
+
+    runValidation,
+    requireSignin,
+    adminMiddleware,
+    create
+);
+router.get("category", list);
+router.get("category/:slug", read);
+router.put(
+    "category/:slug",
+    categoryUpdateValidator,
+    runValidation,
+    requireSignin,
+    adminMiddleware,
+    create
+);
+router.get("category/:slug", requireSignin, adminMiddleware, remove);
+
+module.exports = router;
